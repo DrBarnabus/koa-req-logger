3ee52e71ccfdd49ecfa4c24c10e602566e3d5390
DrBarnabus <drbarnabus@drbarnabus.com>
2018-07-20 21:17:40 +0100

    Implemented the middleware, and also implemented basic tests for the 3 HTTP Headers.

--- index.js
+++ index.js
@@ -1,77 +1,102 @@
 const pino = require('pino');
 const uuidv4 = require('uuid/v4');
 
-const serializers = {
+const stdSerializers = {
   req: require('./lib/req'),
   res: require('./lib/res'),
   err: require('./lib/err')
 };
 
-module.exports = KoaLogger;
+module.exports = class KoaReqLogger {
+  /**
+   * A logging middleware for koa, this middleware also sets the HTTP Date header,
+   * sets the X-Response-Time header to the response time in milliseconds and sets the
+   * X-Request-ID header to either a new uuidv4 or the request id passed into the request
+   * @example
+   * ``` javascript
+   * const app = Koa();
+   * const logger = KoaReqLogger();
+   * app.use(logger.getMiddleware());
+   * app.listen(3000);
+   * ```
+   * @param {object} opts An optional options object
+   * @api public
+   */
+  constructor(opts) {
+    this.middleware = this.middleware.bind(this);
 
-function KoaLogger(opts) {
-  if (!(this instanceof KoaLogger)) {
-    return new KoaLogger(opts);
+    opts = opts || {};
+    opts.serializers = opts.serializers || {};
+    opts.serializers.req = opts.serializers.req || stdSerializers.req;
+    opts.serializers.res = opts.serializers.res || stdSerializers.res;
+    opts.serializers.err = opts.serializers.err || stdSerializers.err;
+
+    this.logger = pino(opts);
   }
-
-  this.opts = opts | {};
-  this.opts.serializers = opts.serializers | serializers;
-
-  this.logger = pino(opts);
 
   /**
    * This function takes a request context and assigns a request id
    * This will either be a newly generated uuidv4 or the X-Request-ID header passed into the request
    * The resulting request id is then set as the X-Request-ID header on the response
    * @param ctx The current koa response context
+   * @api private
    */
-  this.setReqId = function(ctx) {
+  setRequestId(ctx) {
     if (ctx.get('X-Request-ID')) {
       ctx.id = ctx.get('X-Request-ID');
       ctx.set('X-Request-ID', ctx.id);
     } else {
       ctx.id = uuidv4();
-      ctx.set('X-Response-Time', ctx.id);
+      ctx.set('X-Request-ID', ctx.id);
     }
-  };
+  }
 
   /**
    * This function sets the current date as the date header,
    * as well as logging the request and storing the start date for the calculation of the response time later
    * @param ctx The current koa context
+   * @api private
    */
-  this.startRequest = function(ctx) {
+  startRequest(ctx) {
     ctx.start = new Date();
     ctx.set('Date', ctx.start.toUTCString());
 
     ctx.log.info({ req: ctx.req, startDate: ctx.start.toUTCString() }, `${ctx.request.ip} - ${ctx.method} ${ctx.path}`);
-  };
+  }
 
   /**
    * This function calcuates the response time and sets the X-Response-Time header
    * @param ctx The current koa context
+   * @api private
    */
-  this.setResponseTime = function(ctx) {
+  setResponseTime(ctx) {
     let now = new Date();
     ctx.responseTime = now - ctx.start;
     ctx.set('X-Response-Time', `${ctx.responseTime}ms`);
-  };
+  }
 
   /**
    * This function logs the end of a request,
    * it also calls another function to calculate and set the X-Response-Time header
    * @param ctx The current koa context
+   * @api private
    */
-  this.endRequest = function(ctx) {
+  endRequest(ctx) {
     this.setResponseTime(ctx);
 
     ctx.log.info(
       { res: ctx.res, responseTime: ctx.responseTime, startDate: ctx.start.toUTCString() },
       `${ctx.ip} - ${ctx.method} ${ctx.path} - ${ctx.status} ${ctx.responseTime}ms`
     );
-  };
+  }
 
-  this.endRequest = function(e, ctx) {
+  /**
+   * This function logs the end of a request and the error that has been caught,
+   * it also calls another function to calculate and set the X-Response-Time header
+   * @param ctx The current koa context
+   * @api private
+   */
+  endRequestError(e, ctx) {
     this.setResponseTime(ctx);
 
     // Construct error response
@@ -87,26 +112,36 @@ function KoaLogger(opts) {
       { res: ctx.res, err: e, responseTime: ctx.responseTime, startDate: ctx.start.toUTCString() },
       `${ctx.ip} - ${ctx.method} ${ctx.path} - ${ctx.status} ${ctx.responseTime}ms`
     );
-  };
-}
+  }
 
-KoaLogger.prototype.middleware = async function middleware(ctx, next) {
-  // Set the request id
-  this.setReqId(ctx);
+  /**
+   * This function handles all requests and is used as a koa middleware
+   * @param ctx The current koa context
+   * @param next The next function in the middleware stack
+   * @api private
+   */
+  async middleware(ctx, next) {
+    // Set the request id
+    this.setRequestId(ctx);
 
-  // Create a child of the logger with the request id as the key
-  ctx.log = ctx.logger = ctx.req.log = ctx.request.log = ctx.response.log = this.logger.child({ id: ctx.id });
+    // Create a child of the logger with the request id as the key
+    ctx.log = ctx.logger = ctx.req.log = ctx.request.log = ctx.response.log = this.logger.child({ id: ctx.id });
 
-  // Start the request logging
-  this.startRequest(ctx);
+    // Start the request logging
+    this.startRequest(ctx);
 
-  try {
-    await next();
+    try {
+      await next();
 
-    // End the request logging
-    this.endRequest(ctx);
-  } catch (e) {
-    // Handle error and end request logging
-    this.endRequestError(e, ctx);
+      // End the request logging
+      this.endRequest(ctx);
+    } catch (e) {
+      // Handle error and end request logging
+      this.endRequestError(e, ctx);
+    }
+  }
+
+  getMiddleware() {
+    return this.middleware;
   }
 };
---
+++ test/middleware.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const request = require('supertest');
+const Koa = require('koa');
+const KoaReqLogger = require('../');
+
+describe('middleware', () => {
+  // test('Should return a function', () => {
+  //   expect(typeof KoaReqLogger()).toBe('function');
+  // });
+
+  test('Response should include HTTP Date header', async done => {
+    // Setup
+    const app = new Koa();
+    const logger = new KoaReqLogger();
+    app.use(logger.getMiddleware());
+
+    app.use((ctx, next) => {
+      ctx.status = 200;
+      ctx.body = {
+        data: 'Hello World!'
+      };
+    });
+
+    let server = app.listen();
+
+    // Test
+    const response = await request(server).get('/');
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual('application/json');
+    expect(response.body.data).toEqual('Hello World!');
+    expect(response.headers['date']).toBeDefined();
+
+    // Teardown
+    server.close();
+    done();
+  });
+
+  test('Response should include HTTP X-Response-Time header', async done => {
+    // Setup
+    const app = new Koa();
+    const logger = new KoaReqLogger();
+    app.use(logger.getMiddleware());
+
+    app.use((ctx, next) => {
+      ctx.status = 200;
+      ctx.body = {
+        data: 'Hello World!'
+      };
+    });
+
+    let server = app.listen();
+
+    // Test
+    const response = await request(server).get('/');
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual('application/json');
+    expect(response.body.data).toEqual('Hello World!');
+    expect(response.headers['x-response-time']).toBeDefined();
+
+    // Teardown
+    server.close();
+    done();
+  });
+
+  test('Response should include HTTP X-Request-ID header', async done => {
+    // Setup
+    const app = new Koa();
+    const logger = new KoaReqLogger();
+    app.use(logger.getMiddleware());
+
+    app.use((ctx, next) => {
+      ctx.status = 200;
+      ctx.body = {
+        data: 'Hello World!'
+      };
+    });
+
+    let server = app.listen();
+
+    // Test
+    const response = await request(server).get('/');
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual('application/json');
+    expect(response.body.data).toEqual('Hello World!');
+    expect(response.headers['x-request-id']).toBeDefined();
+
+    // Teardown
+    server.close();
+    done();
+  });
+
+  test('When a X-Request-ID is provided in the request, the same value should be sent back as the HTTP X-Request-ID header', async done => {
+    // Setup
+    const app = new Koa();
+    const logger = new KoaReqLogger();
+    app.use(logger.getMiddleware());
+
+    app.use((ctx, next) => {
+      ctx.status = 200;
+      ctx.body = {
+        data: 'Hello World!'
+      };
+    });
+
+    let server = app.listen();
+
+    // Test
+    const response = await request(server).get('/').set('X-Request-ID', 'test-value');
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual('application/json');
+    expect(response.body.data).toEqual('Hello World!');
+    expect(response.headers['x-request-id']).toBeDefined();
+    expect(response.headers['x-request-id']).toEqual('test-value');
+
+    // Teardown
+    server.close();
+    done();
+  });
+});
